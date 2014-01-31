e8e41beaedec4b86092e94311ee4116a13ce28d2
Lee Hinman <lee@writequit.org>
2014-01-31 12:09:30 -0700

    better face keywording

--- es-mode.el
+++ es-mode.el
@@ -43,11 +43,50 @@
     map)
   "Keymap for ES major mode")
 
+(defconst es-top-level-fields '("facets" "filter" "post_filter" "query"))
+(defconst es-keywords '("fields" "from" "size"))
+(defconst es-http-builtins '("DELETE" "GET" "POST" "PUT"))
+(defconst es-parent-types '("and" "bool" "filtered" "must"
+                            "must_not" "not" "or" "should"))
+(defconst es-query-types '("boosting" "common" "constant_score"
+                           "custom_boost_factor" "custom_filters_score"
+                           "custom_score" "dismax" "function_score" "fuzzy"
+                           "fuzzy_like_this" "fuzzy_like_this_field"
+                           "geo_shape" "has_child" "has_parent" "ids"
+                           "indices" "match" "match_all" "match_phrase"
+                           "match_phrase_prefix" "more_like_this"
+                           "more_like_this_field" "multi_match" "nested"
+                           "prefix" "query_string" "range" "regexp"
+                           "simple_query_string" "span_first"
+                           "span_multi_term" "span_near" "span_not" "span_or"
+                           "span_term" "term" "terms" "text" "top_children"
+                           "wildcard"))
+(defconst es-facet-types '("date_histogram" "geo_distance" "histogram"
+                           "statistical" "terms_stats"))
+
 (defconst es-font-lock-keywords
-  (list
-   '("'\\(.+?\\)'" . font-lock-string-face)
-   '("\\<\\(true\\|false\\)\\>" . font-lock-constant-face)
-   '("\\<\\(#.*\\)\\>" . font-lock-comment-face))
+  (eval-when-compile
+    `(("'\\(.+?\\)'" . font-lock-string-face)
+      ("\\<\\(true\\|false\\)\\>" . font-lock-constant-face)
+      ("\\<\\(#.*\\)\\>" . font-lock-comment-face)
+      ;; top-level fields containing leaf nodes
+      (,(concat "\"\\(" (regexp-opt es-top-level-fields) "\\)\"")
+       (1 font-lock-constant-face t))
+      ;; keywords for fields usually specified
+      (,(concat "\"\\(" (regexp-opt es-keywords) "\\)\"")
+       (1 font-lock-keyword-face t))
+      ;; builtins for REST
+      (,(concat "-X\\(" (regexp-opt es-http-builtins) "\\)")
+       (1 font-lock-builtin-face t))
+      ;; types (parent queries containing sub queries)
+      (,(concat "\"\\(" (regexp-opt es-parent-types) "\\)\"")
+       (1 font-lock-type-face t))
+      ;; query types (leaf nodes)
+      (,(concat "\"\\(" (regexp-opt es-query-types) "\\)\"")
+       (1 font-lock-function-name-face t))
+      ;; facet types (leaf nodes)
+      (,(concat "\"\\(" (regexp-opt es-facet-types) "\\)\"")
+       (1 font-lock-function-name-face t))))
   "Highlighting expressions for ES mode")
 
 (defun es-indent-line ()
@@ -65,82 +104,6 @@
     (modify-syntax-entry ?\n "> b" st)
     st)
   "Syntax table for ES mode.")
-
-;; Constants, top-level fields
-(font-lock-add-keywords 'es-mode '(("\"\\(facets\\)\"" 1 'font-lock-constant-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(filter\\)\"" 1 'font-lock-constant-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(post_filter\\)\"" 1 'font-lock-constant-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(query\\)\"" 1 'font-lock-constant-face t)))
-
-;; Keywords
-(font-lock-add-keywords 'es-mode '(("\"\\(fields\\)\"" 1 'font-lock-keyword-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(from\\)\"" 1 'font-lock-keyword-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(size\\)\"" 1 'font-lock-keyword-face t)))
-
-
-;; Builtins
-(font-lock-add-keywords 'es-mode '(("-X\\(DELETE\\)" 1 'font-lock-builtin-face t)))
-(font-lock-add-keywords 'es-mode '(("-X\\(GET\\)" 1 'font-lock-builtin-face t)))
-(font-lock-add-keywords 'es-mode '(("-X\\(POST\\)" 1 'font-lock-builtin-face t)))
-(font-lock-add-keywords 'es-mode '(("-X\\(PUT\\)" 1 'font-lock-builtin-face t)))
-
-;; Types (parent nodes)
-(font-lock-add-keywords 'es-mode '(("\"\\(and\\)\"" 1 'font-lock-type-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(bool\\)\"" 1 'font-lock-type-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(filtered\\)\"" 1 'font-lock-type-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(must\\)\"" 1 'font-lock-type-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(must_not\\)\"" 1 'font-lock-type-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(not\\)\"" 1 'font-lock-type-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(or\\)\"" 1 'font-lock-type-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(should\\)\"" 1 'font-lock-type-face t)))
-
-;; Functions (query types)
-(font-lock-add-keywords 'es-mode '(("\"\\(boosting\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(common\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(constant_score\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(custom_boost_factor\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(custom_filters_score\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(custom_score\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(dismax\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(function_score\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(fuzzy\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(fuzzy_like_this\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(fuzzy_like_this_field\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(geo_shape\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(has_child\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(has_parent\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(ids\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(indices\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(match\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(match_all\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(match_phrase\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(match_phrase_prefix\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(more_like_this\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(more_like_this_field\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(multi_match\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(nested\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(prefix\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(query_string\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(range\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(regexp\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(simple_query_string\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(span_first\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(span_multi_term\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(span_near\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(span_not\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(span_or\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(span_term\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(term\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(terms\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(text\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(top_children\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(wildcard\\)\"" 1 'font-lock-function-name-face t)))
-;; Functions (facet types)
-(font-lock-add-keywords 'es-mode '(("\"\\(date_histogram\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(geo_distance\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(histogram\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(statistical\\)\"" 1 'font-lock-function-name-face t)))
-(font-lock-add-keywords 'es-mode '(("\"\\(terms_stats\\)\"" 1 'font-lock-function-name-face t)))
 
 (define-derived-mode es-mode prog-mode "ES"
   "Major mode for editing curl ES scripts, similar to both sh-mode and js-mode."
