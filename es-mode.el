150dfa2e278aced0741615df8e8101464f492644
Lee Hinman <lee@writequit.org>
2014-03-27 16:29:23 -0600

    bump to 2.1.0 now that 2.0.0 has been tagged

--- es-mode.el
+++ es-mode.el
@@ -5,7 +5,7 @@
 
 ;; Author: Lee Hinman <lee@writequit.org>
 ;; URL: http://www.github.com/dakrone/es-mode
-;; Version: 2.0.0
+;; Version: 2.1.0
 ;; Keywords: elasticsearch
 
 ;; This file is not part of GNU Emacs.
