9fb395996316c140f3a6c77afb10dcd37cb49126
Lee Hinman <lee@writequit.org>
2017-02-13 12:37:11 -0700

    Add s.el as a dependency
    Resolves #38

--- es-cc.el
+++ es-cc.el
@@ -6,7 +6,7 @@
 ;; URL: http://www.github.com/dakrone/es-mode
 ;; Version: 1.0.0
 ;; Keywords: elasticsearch
-;; Package-Requires: ((dash "2.11.0") (cl-lib "0.5") (spark "1.0"))
+;; Package-Requires: ((dash "2.11.0") (cl-lib "0.5") (spark "1.0") (s 1.11.0))
 
 ;; This file is not part of GNU Emacs.
 
